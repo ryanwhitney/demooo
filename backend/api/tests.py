8d500c75598d2185126efb46ec9847d351a09bb7
Ryan Whitney <ryan.whitney@me.com>
2025-04-11 09:11:17 -0700

    update tests to use graphql queries

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,33 +1,154 @@
-from django.test import TestCase
 from django.contrib.auth import get_user_model
-from graphene.test import Client
-from .schema import schema
+from graphql_jwt.testcases import JSONWebTokenTestCase
 
 User = get_user_model()
 
-class APITests(TestCase):
+
+class APITests(JSONWebTokenTestCase):
     def setUp(self):
-        self.client = Client(schema)
-        self.user = User.objects.create_user(
-            username='testuser',
-            email='test@example.com',
-            password='testpass123'
-        )
+        super().setUp()
 
-    def test_user_creation(self):
-        """Test that a user can be created"""
-        self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(self.user.username, 'testuser')
-        self.assertEqual(self.user.email, 'test@example.com')
-
-    def test_login_mutation(self):
-        """Test the login mutation"""
+    def test_create_user_mutation(self):
+        """Test user creation through GraphQL mutation"""
         query = '''
+        mutation {
+            createUser(
+                username: "testuser",
+                email: "test@example.com",
+                password: "testpass123"
+            ) {
+                user {
+                    id
+                    username
+                    email
+                }
+            }
+        }
+        '''
+        response = self.client.execute(query)
+        
+        self.assertIsNotNone(response.data['createUser']['user'])
+        created_user = response.data['createUser']['user']
+        self.assertEqual(created_user['username'], 'testuser')
+        self.assertEqual(created_user['email'], 'test@example.com')
+        self.assertEqual(User.objects.count(), 1)
+
+    def test_graphql_login_mutation(self):
+        """Test the login mutation"""
+        # Create user
+        create_query = '''
+        mutation {
+            createUser(
+                username: "testuser",
+                email: "test@example.com",
+                password: "testpass123"
+            ) {
+                user {
+                    username
+                }
+            }
+        }
+        '''
+        self.client.execute(create_query)
+
+        # Login
+        login_query = '''
         mutation {
             tokenAuth(username: "testuser", password: "testpass123") {
                 token
             }
         }
         '''
-        result = self.client.execute(query)
-        self.assertIsNotNone(result['data']['tokenAuth']['token'])
+        response = self.client.execute(login_query)
+        self.assertIsNotNone(response.data['tokenAuth']['token'])
+
+    def test_login_mutation_wrong_password(self):
+        """Test login mutation with wrong password"""
+        # Create user
+        create_query = '''
+        mutation {
+            createUser(
+                username: "testuser",
+                email: "test@example.com",
+                password: "testpass123"
+            ) {
+                user {
+                    username
+                }
+            }
+        }
+        '''
+        self.client.execute(create_query)
+
+        # Login with bad password
+        login_query = '''
+        mutation {
+            tokenAuth(username: "testuser", password: "wrongpassword") {
+                token
+            }
+        }
+        '''
+        response = self.client.execute(login_query)
+        self.assertIsNotNone(response.errors)
+        self.assertIn("Please enter valid credentials", str(response.errors))
+
+    def test_login_mutation_nonexistent_user(self):
+        """Test login mutation with nonexistent user"""
+        login_query = '''
+        mutation {
+            tokenAuth(username: "nonexistent", password: "testpass123") {
+                token
+            }
+        }
+        '''
+        response = self.client.execute(login_query)
+        self.assertIsNotNone(response.errors)
+        self.assertIn("Please enter valid credentials", str(response.errors))
+
+    def test_authenticated_query(self):
+        """Test that authenticated queries work"""
+        # Create user
+        create_query = '''
+        mutation {
+            createUser(
+                username: "testuser",
+                email: "test@example.com",
+                password: "testpass123"
+            ) {
+                user {
+                    username
+                }
+            }
+        }
+        '''
+        self.client.execute(create_query)
+
+        # Auth and try query
+        user = User.objects.get(username='testuser')
+        self.client.authenticate(user)
+
+        query = '''
+        query {
+            me {
+                username
+                email
+            }
+        }
+        '''
+        response = self.client.execute(query)
+        self.assertEqual(response.data['me']['username'], 'testuser')
+        self.assertEqual(response.data['me']['email'], 'test@example.com')
+
+    def test_authenticated_query_unauthorized(self):
+        """Test that unauthenticated queries fail"""
+        query = '''
+        query {
+            me {
+                username
+                email
+            }
+        }
+        '''
+        response = self.client.execute(query)
+        self.assertIsNotNone(response.errors)
+        self.assertIn("You do not have permission to perform this action", str(response.errors))
