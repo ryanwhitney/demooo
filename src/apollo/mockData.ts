fe8eda531b56d950103d568619c69fb6c3d2fe05
Ryan Whitney <ryan.whitney@me.com>
2025-04-13 23:49:37 -0700

    fix implied optionals by removing incomplete data

--- src/apollo/mockData.ts
+++ src/apollo/mockData.ts
@@ -1,36 +1,22 @@
 export const mockData = {
 	tracks: [
 		{
-			id: "feakop-feaw-fk-af",
+			id: "666",
 			title: "side_a.aif",
-			artist: "ryanwhitney",
+			artist: {
+				id: "111",
+				username: "ryanwhitney",
+			},
 			titleSlug: "side-a-aig",
+			audioFile:
+				"http://localhost:8000/media/audio/1f2b65b4-32cc-4b88-af56-71b492d19e73/57566347-823d-44d6-827f-2069bb9b0b8b/57566347-823d-44d6-827f-2069bb9b0b8b.aif",
 			description: "recorded drunk in a hotel room in new orleans.",
 			duration: 120,
 			createdAt: "2025-04-04",
 			recordedAt: "2025-04-04",
-			albumArt: "https://picsum.photos/300/300.jpg",
-			user: {
-				id: "long-uuidstring",
-				username: "ryanwhitney",
-			},
-			tags: "synth, electronic, op-1",
-		},
-		{
-			id: "fk03-fwea-feawfl-feaw",
-			title: "Naive Drum Machine",
-			artist: "granta",
-			titleSlug: "side-a-aig",
-			description: "",
-			duration: 161,
-			createdAt: "2025-01-01",
-			recordedAt: "2025-01-01",
-			albumArt: "https://picsum.photos/300/300.jpg",
-			user: {
-				id: "long-awffjoaw-fewa",
-				username: "ryanwhitney",
-			},
-			tags: "drum machine, electronic",
-		},
+			audioWaveformData:
+				"[0.32, 0.45, 1.0, 0.54, 0.42, 0.47, 0.43, 0.26, 0.35, 0.54, 0.26, 0.37, 0.3, 0.98, 0.64, 0.43, 0.45, 0.48, 0.26, 0.26, 0.47, 0.44, 0.37, 0.27, 0.88, 0.76, 0.43, 0.42, 0.44, 0.39, 0.24, 0.45, 0.45, 0.36, 0.26, 0.5, 0.99, 0.53, 0.41, 0.45, 0.42, 0.26, 0.39, 0.51, 0.26, 0.37, 0.36, 0.96, 0.63, 0.44, 0.46, 0.46, 0.25, 0.31, 0.55, 0.27, 0.28, 0.14, 0.23, 0.12, 0.09, 0.07, 0.07, 0.05, 0.04, 0.04, 0.04, 0.05, 0.04, 0.07, 0.08, 0.07, 0.07, 0.07, 0.06, 0.05, 0.07, 0.07, 0.05, 0.03]",
+			audioWaveformResolution: 80,
+		}
 	],
 };
