ca0d9994c5302fd464269e1d7d6f2f8b57d50338
Ryan Whitney <ryan.whitney@me.com>
2025-04-16 12:55:32 -0700

    use toggleIcon for playbutton

---
+++ src/components/audioPlayer/components/playButton/PlayButton.stories.tsx
@@ -0,0 +1,18 @@
+import type { StoryObj } from "@storybook/react";
+import PlayButton from "./PlayButton";
+
+const meta = {
+	title: "Components/PlayButton",
+	component: PlayButton,
+	tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+	args: {
+		isPlaying: false,
+	},
+};
