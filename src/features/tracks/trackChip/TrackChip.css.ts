eb019b7fc67fc58b1873ec42867040deb6f2f157
Ryan Whitney <ryan.whitney@me.com>
2025-05-06 00:21:57 -0700

    keep hover state when playing + remove need for globalstyles

--- src/features/tracks/trackChip/TrackChip.css.ts
+++ src/features/tracks/trackChip/TrackChip.css.ts
@@ -1,157 +1,154 @@
-import { tokens } from "@/styles/tokens";
-import { globalStyle, style } from "@vanilla-extract/css";
+import { tokens } from '@/styles/tokens'
+import { style } from '@vanilla-extract/css'
+import { recipe } from '@vanilla-extract/recipes'
 
 export const trackChipWrapper = style({
-	width: 148,
-	padding: tokens.space.sm,
-	border: '1px solid transparent',
-	borderRadius: tokens.radii.lg,
-	transition: "all 0.2s ease-in-out",
-	textDecoration: "none",
-	display:'flex',
-	flexDirection: 'column-reverse',
-	backgroundColor: tokens.colors.quaternaryDark,
-	borderColor: tokens.colors.quaternaryDark,
+  width: 148,
+  padding: tokens.space.sm,
+  border: '1px solid transparent',
+  borderRadius: tokens.radii.lg,
+  transition: 'all 0.2s ease-in-out',
+  textDecoration: 'none',
+  display: 'flex',
+  flexDirection: 'column-reverse',
+  backgroundColor: tokens.colors.quaternaryDark,
+  borderColor: tokens.colors.quaternaryDark,
 
-	":hover": {
-		borderColor: tokens.colors.tertiaryDark,
-	},
-});
+  ':hover': {
+    borderColor: tokens.colors.tertiaryDark,
+  },
+})
 
 export const waveformWrapper = style({
-	backgroundColor: tokens.colors.quaternaryDark,
-	borderRadius: tokens.radii.lg,
-	display: "flex",
-	justifyContent: "center",
-	padding: "16px 8px",
-	position: 'relative',
-});
-
-export const trackChipPlayButton = style({
-	width: 24,
-	height: 24,
-	padding: 8,
-	alignSelf: 'center',
-	justifySelf: 'center',
-	transition: 'opacity 150ms ease', 
-	zIndex: 4,
-	opacity: 0,
-	borderRadius: tokens.radii.full,
-	':focus':{
-		opacity: 1
-	},
-	':focus-visible':{
-		backgroundColor: tokens.colors.tintColor, // more obvious as the waveform doesn't dim like hover
-	},
-	'::before':{
-		content: '\ ',
-		position: 'absolute',
-		top: 0,
-		left: 0,
-		right: 0,
-		bottom: 0,
-		inset: 0,
-		zIndex: 5,
-	},
+  backgroundColor: tokens.colors.quaternaryDark,
+  borderRadius: tokens.radii.lg,
+  display: 'flex',
+  justifyContent: 'center',
+  padding: '16px 8px',
+  position: 'relative',
 })
 
-export const waveformElement = style({
-	opacity: 1,
-	position: 'absolute',
-	transition: 'opacity 200ms ease', 
-	transform: 'translate3d(0, 0, 0)', // fix safari jitter
-});
+export const trackChipPlayButton = recipe({
+  base: {
+    width: 24,
+    height: 24,
+    padding: 8,
+    alignSelf: 'center',
+    justifySelf: 'center',
+    transition: 'opacity 150ms ease',
+    zIndex: 4,
+    opacity: 0,
+    borderRadius: tokens.radii.full,
+    ':focus': {
+      opacity: 1,
+    },
+    ':focus-visible': {
+      backgroundColor: tokens.colors.tintColor, // more obvious as the waveform doesn't dim like hover
+    },
+    '::before': {
+      content: ' ',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      inset: 0,
+      zIndex: 5,
+    },
+    selectors: {
+      [`${waveformWrapper}:hover &`]: {
+        opacity: 1,
+      },
+      [`${trackChipWrapper}:focus &`]: {
+        opacity: 1,
+      },
+      [`${trackChipWrapper}:focus-visible &`]: {
+        opacity: 1,
+      },
+    },
+  },
+  variants: {
+    isPlaying: {
+      true: {
+        opacity: 1,
+      },
+    },
+  },
+})
 
-// Add this globalStyle definition to create the group hover effect
-globalStyle(`${waveformWrapper}:hover ${trackChipPlayButton}`, {
-  opacity: 1,
-});
-
-// Add this globalStyle definition to create the group hover effect
-globalStyle(`${trackChipWrapper}:focus ${trackChipPlayButton}`, {
-  opacity: 1,
-});
-
-// Add this globalStyle definition to create the group hover effect
-globalStyle(`${trackChipWrapper}:focus-visible ${trackChipPlayButton}`, {
-  backgroundColor: tokens.colors.primary,
-	
-});
-
-globalStyle(`${waveformWrapper}:hover ${waveformElement}`, {
-  opacity: 0.2
-});
-
-globalStyle(`${trackChipWrapper}:focus ${waveformElement}`, {
-  opacity: 0.2
-});
+export const waveformElement = recipe({
+  base: {
+    opacity: 1,
+    position: 'absolute',
+    transition: 'opacity 200ms ease',
+    transform: 'translate3d(0, 0, 0)', // fix safari jitter
+    selectors: {
+      [`${waveformWrapper}:hover &`]: {
+        opacity: 0.2,
+      },
+      [`${trackChipWrapper}:focus &`]: {
+        opacity: 0.2,
+      },
+    },
+  },
+  variants: {
+    isPlaying: {
+      true: {
+        opacity: 0.2,
+      },
+    },
+  },
+})
 
 export const trackText = style({
-	fontSize: tokens.fontSizes.sm,
-	display: "flex",
-	flexDirection: "column",
-	// gap: tokens.space.xs,
-	paddingTop: tokens.space.md,
-});
-
-export const trackTitle = style({
-	cursor: "pointer",
-	color: tokens.colors.primary,
-	textDecoration: "none",
-	zIndex: 2,
-	// background: 'red',
-	padding: tokens.space.sm,
-	position: 'relative',
-	fontWeight: 400,
-	// transition: 'font-weight 200ms ease',
-	// account for line 2 somehow showing below
-	lineHeight: 1.8,
-	marginBottom: -2,
-	// padding applies to linebreaks
-	WebkitBoxDecorationBreak: 'clone',
-	boxDecorationBreak: 'clone',
-	// line clmp
-	WebkitLineClamp: 1,
-	display: '-webkit-box',
-  WebkitBoxOrient: 'vertical',
-	textOverflow: 'ellipsis',
-	lineBreak: 'anywhere',
-	borderRadius: tokens.radii.sm,
-	overflow: 'hidden',
-	':hover':{
-		fontWeight: 600,
-	}
-});
-
-export const trackArtistContainer = style({
-	cursor: "pointer",
-	color: tokens.colors.secondary,
-	textDecoration: "none",
-	display: 'flex',
-	lineHeight: 1,
-	borderRadius: tokens.radii.sm,
-	// background: 'blue',
-	alignItems: 'center',
-	gap: 5,
-	// width: 'fit-content',
-	padding: tokens.space.sm,
-	position: 'relative',
-	// transition: "color 0.1s ease-in-out",
-	zIndex: 2,
-	':hover':{
-		color: tokens.colors.primary,
-	}
+  fontSize: tokens.fontSizes.sm,
+  display: 'flex',
+  flexDirection: 'column',
+  paddingTop: tokens.space.md,
 })
 
-export const trackArtist = style({
-	// padding applies to linebreaks
-	WebkitBoxDecorationBreak: 'clone',
-	boxDecorationBreak: 'clone',
-	// line clmp
-	WebkitLineClamp: 1,
-	display: '-webkit-box',
+export const trackTitle = style({
+  cursor: 'pointer',
+  color: tokens.colors.primary,
+  textDecoration: 'none',
+  zIndex: 2,
+  padding: tokens.space.sm,
+  position: 'relative',
+  fontWeight: 400,
+  // account for line 2 somehow showing below
+  lineHeight: 1.8,
+  marginBottom: -2,
+  // make padding apply to linebreaks
+  WebkitBoxDecorationBreak: 'clone',
+  boxDecorationBreak: 'clone',
+  // all required for line clamp
+  WebkitLineClamp: 1,
+  display: '-webkit-box',
   WebkitBoxOrient: 'vertical',
-	textOverflow: 'ellipsis',
-	lineBreak: 'anywhere',
-	overflow: 'hidden',
-});
+  textOverflow: 'ellipsis',
+  lineBreak: 'anywhere',
+  borderRadius: tokens.radii.sm,
+  overflow: 'hidden',
+  ':hover': {
+    fontWeight: 600,
+  },
+})
+
+export const trackArtistContainer = style({
+  cursor: 'pointer',
+  color: tokens.colors.secondary,
+  textDecoration: 'none',
+  display: 'flex',
+  lineHeight: 1,
+  borderRadius: tokens.radii.sm,
+  alignItems: 'center',
+  gap: 5,
+  padding: tokens.space.sm,
+  position: 'relative',
+  zIndex: 2,
+  ':hover': {
+    color: tokens.colors.primary,
+  },
+})
+
+export const trackArtist = style({})
--- src/features/tracks/trackChip/TrackChip.tsx
+++ src/features/tracks/trackChip/TrackChip.tsx
@@ -1,15 +1,6 @@
 import type { Track } from "@/types/track";
 import { Link } from "react-router";
-import {
-	trackArtist,
-	trackArtistContainer,
-	trackChipPlayButton,
-	trackChipWrapper,
-	trackText,
-	trackTitle,
-	waveformElement,
-	waveformWrapper,
-} from "./TrackChip.css";
+import * as style from "./TrackChip.css";
 import PlayButton from "@/components/audioPlayer/components/playButton/PlayButton";
 import { useAudio } from "@/providers/AudioProvider";
 import { memo, useCallback, useMemo } from "react";
@@ -41,36 +32,39 @@ const TrackChip = memo(function TrackChip({ track }: { track: Track }) {
 	}, [audio, isCurrentTrack, isPlaying, track]);
 
 	return (
-		<article className={trackChipWrapper}>
-			<div className={trackText}>
+		<article className={style.trackChipWrapper}>
+			<div className={style.trackText}>
 				<strong>
 					<Link
-						className={trackTitle}
+						className={style.trackTitle}
 						to={`/${track.artist.username}/track/${track.titleSlug}`}
 					>
 						{track.title}
 					</Link>
 				</strong>
-				<Link className={trackArtistContainer} to={`/${track.artist.username}`}>
+				<Link
+					className={style.trackArtistContainer}
+					to={`/${track.artist.username}`}
+				>
 					<ProfilePhoto
 						ariaHidden={true}
 						height={16}
 						width={16}
 						profile={track.artist.profile}
 					/>
-					<span className={trackArtist}>
+					<span className={style.trackArtist}>
 						{track.artist.profile.name || track.artist.username}
 					</span>
 				</Link>
 			</div>
-			<div className={waveformWrapper}>
+			<div className={style.waveformWrapper}>
 				<PlayButton
-					className={trackChipPlayButton}
+					className={style.trackChipPlayButton({ isPlaying: isPlaying })}
 					isPlaying={isPlaying}
 					onClick={handleClick}
 					color="white"
 				/>
-				<div className={waveformElement}>
+				<div className={style.waveformElement({ isPlaying: isPlaying })}>
 					<Waveform
 						data={track.audioWaveformData}
 						width={91}
